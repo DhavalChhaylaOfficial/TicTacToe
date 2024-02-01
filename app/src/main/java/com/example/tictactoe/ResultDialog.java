44062ca288c435d1581fef5996486717e58093d2
Dhaval Chhayla <82968230+DhavalChhaylaOfficial@users.noreply.github.com>
2024-02-01 11:30:03 +0530

    first

---
+++ app/src/main/java/com/example/tictactoe/ResultDialog.java
@@ -0,0 +1,54 @@
+package com.example.tictactoe;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+import java.util.Objects;
+
+public class ResultDialog extends Dialog {
+    private final String message;
+    private final MainActivity mainActivity;
+
+    public ResultDialog(@NonNull Context context, String message, MainActivity mainActivity) {
+        super(context);
+        this.message = message;
+        this.mainActivity = mainActivity;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_dialog);
+
+        TextView messageText = findViewById(R.id.messageText);
+        Button startAgainButton = findViewById(R.id.startAgainButton);
+        LottieAnimationView animationView1 = findViewById(R.id.otpvrfy);
+
+        messageText.setText(message);
+
+        if(message.equals("Match Draw")) {
+            animationView1.setAnimation(R.raw.tie);
+        }
+
+        else {
+            animationView1.setAnimation(R.raw.trophy);
+        }
+
+        startAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainActivity.restartMatch();
+                dismiss();
+            }
+        });
+    }
+}
